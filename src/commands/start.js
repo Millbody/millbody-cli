9d3f60e6297d44f8dcab296742f7ce2afb853abf
jaimeneto85 <jaimeneto85@gmail.com>
2020-02-10 18:20:30 -0300

    resertando cache do servidor

--- src/commands/start.js
+++ src/commands/start.js
@@ -4,8 +4,8 @@ import { executeCommand, checkFolderBySource } from '../functions';
 export default function executStart(options){
     const source = checkFolderBySource(options.source);
     if(source == 'theme'){
-        executeCommand("cp millbodyApp.json ../../../ && cd ../../../ && npm start");
+        executeCommand("cp millbodyApp.json ../../../ && cd ../../../ && npm start --reset-cache");
     }else{
-        executeCommand("npm start");
+        executeCommand("npm start --reset-cache");
     }
 }
