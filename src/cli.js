2c60b66c09c929b31e1457c3b7f84280430595b2
Eduardo Queiroz <eduardoqueiroz@Eduardos-iMac.local>
2020-05-12 11:24:45 -0300

    add envoriment to start commands, test command AB#331

--- src/cli.js
+++ src/cli.js
@@ -1,74 +1,78 @@
-import arg from 'arg';
-import inquirer from 'inquirer';
+import arg from "arg";
+import inquirer from "inquirer";
 
-import executeStart         from './commands/start';
-import executeCodePush      from './commands/codepush';
-import executeRunAndroid    from './commands/runAndroid';
-import executeBuildAndroid  from './commands/buildAndroid';
-import executeRunIos        from './commands/runIos';
+import executeStart from "./commands/start";
+import executeTest from "./commands/test";
+import executeCodePush from "./commands/codepush";
+import executeRunAndroid from "./commands/runAndroid";
+import executeBuildAndroid from "./commands/buildAndroid";
+import executeRunIos from "./commands/runIos";
 
-function parseArgumentsIntoOptions(rawArgs){
-    const args = arg(
-        {
-            '--source': String,
-            '-s': '--source',
-        },
-        {
-            argv: rawArgs.slice(2)
-        }
-    );
-    return {
-        template: args._[0],
-        source: args['--source'] == 'root' ? 'root' : 'theme'
+function parseArgumentsIntoOptions(rawArgs) {
+  const args = arg(
+    {
+      "--source": String,
+      "-s": "--source",
+    },
+    {
+      argv: rawArgs.slice(2),
     }
+  );
+  return {
+    template: args._[0],
+    source: args["--source"] == "root" ? "root" : "theme",
+  };
 }
 
-async function promptForMissingOptions(options){
-    const defaultTemplate = 'start';
-    if(options.skipPrompts){
-        return {
-            ...options,
-            template: options.template || defaultTemplate
-        }
-    }
-
-    const questions = [];
-    if(!options.template){
-        questions.push({
-            type: 'list',
-            name: 'template',
-            message: 'Escolha uma opção de comando para rodar: ',
-            choices: ['new', 'start', 'run-ios', 'run-android', 'codepush'],
-            default: defaultTemplate,
-        })
-    }
-
-    const answers = await inquirer.prompt(questions);
+async function promptForMissingOptions(options) {
+  const defaultTemplate = "start";
+  if (options.skipPrompts) {
     return {
-        ...options,
-        template: options.template || answers.template,
-    }
+      ...options,
+      template: options.template || defaultTemplate,
+    };
+  }
+
+  const questions = [];
+  if (!options.template) {
+    questions.push({
+      type: "list",
+      name: "template",
+      message: "Escolha uma opção de comando para rodar: ",
+      choices: ["new", "start", "run-ios", "run-android", "codepush"],
+      default: defaultTemplate,
+    });
+  }
+
+  const answers = await inquirer.prompt(questions);
+  return {
+    ...options,
+    template: options.template || answers.template,
+  };
 }
 
-export async function cli(args){
-    let options = parseArgumentsIntoOptions(args);
-    options = await promptForMissingOptions(options);
-    const { template, source } = options;
-    switch(template){
-        case 'start':
-            executeStart(options);
-            break;
-        case 'codepush':
-            executeCodePush(options);
-            break;
-        case 'run-android':
-            executeRunAndroid(options);
-            break;
-        case 'run-ios':
-            executeRunIos(options);
-            break;
-        case 'build-android':
-            executeBuildAndroid(options);
-            break;
-    }
+export async function cli(args) {
+  let options = parseArgumentsIntoOptions(args);
+  options = await promptForMissingOptions(options);
+  const { template, source } = options;
+  switch (template) {
+    case "test":
+      executeTest(options);
+      break;
+    case "start":
+      executeStart(options);
+      break;
+    case "codepush":
+      executeCodePush(options);
+      break;
+    case "run-android":
+      executeRunAndroid(options);
+      break;
+    case "run-ios":
+      executeRunIos(options);
+      break;
+    case "build-android":
+      executeBuildAndroid(options);
+      break;
+  }
 }
