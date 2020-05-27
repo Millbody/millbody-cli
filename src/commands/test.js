4fe7a9f2e5acb3c19f79e039f9bb31f9b702faf6
Eduardo Queiroz <eduqueiroz21@gmail.com>
2020-05-27 14:14:21 -0300

    add verification to test command

--- src/commands/test.js
+++ src/commands/test.js
@@ -4,15 +4,20 @@ import { executeCommand, checkFolderBySource } from "../functions";
 export default function executeRunAndroid(options) {
   const source = checkFolderBySource(options.source);
   const millbodyApp = JSON.parse(fs.readFileSync("millbodyApp.json"));
+  const testFileExists = fs.existsSync("index.test.js");
   console.log("Rodando APP " + millbodyApp.account + " no Android");
-  if (source == "theme") {
-    executeCommand(
-      "cd ../../../ && cd android/ && ./gradlew clean && cd .. && ENVFILE=.env.test react-native run-android"
-    );
-  } else {
-    executeCommand(
-      "cd android/ && ./gradlew clean && cd .. && ENVFILE=.env.test react-native run-android"
-    );
+  if(!testFileExists){
+    console.log("Arquivo index.test.js não existe por favor verifique o index.test.default e crie o arquivo");
+  }else {
+    if (source == "theme") {
+      executeCommand(
+        "cd ../../../ && cd android/ && ./gradlew clean && cd .. && ENVFILE=.env.test react-native run-android"
+      );
+    } else {
+      executeCommand(
+        "cd android/ && ./gradlew clean && cd .. && ENVFILE=.env.test react-native run-android"
+      );
+    }
+    console.log("Limpando cache e rodando android...");
   }
-  console.log("Limpando cache e rodando android...");
 }
