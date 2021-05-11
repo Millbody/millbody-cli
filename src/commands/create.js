e2f2178b3259472a351ed89c09b3ce8efdd31edc
Jaime Lima Neto <jaimeneto85@gmail.com>
2021-05-11 13:30:00 -0300

    create app by accountname

--- src/commands/create.js
+++ src/commands/create.js
@@ -12,7 +12,7 @@ export default async function create(options){
     const rootFolder = process.cwd();
     const millbodyJSON = await getAppAccount(options.account)
     if(millbodyJSON.message) return console.log(millbodyJSON.message);
-    if(!millbodyJSON.versionLabel) millbodyJSON.versionLabel = "2.9"
+    if(!millbodyJSON.versionLabel) millbodyJSON.versionLabel = "3.0"
     const {themeName} = millbodyJSON;
     const themeFolder = `${rootFolder}/mbContent/themes/${themeName}`
     console.log('Criando o setup millbody na raiz do projeto')
@@ -36,11 +36,19 @@ export default async function create(options){
         if(err) console.log(err);
       }
     );
+    console.log('desligando qualquer react native server');
+    executeCommand(`kill -9 'lsof -t -i:8081'`);
     console.log('Executando as configurações do MillbodyApp');
     await executeCommand(`cd ${rootFolder} && node millbodyapp.config.js`);
+    console.log('Fechando o Xcode...')
+    executeCommand(`killall Xcode`);
     console.log('Setup de chave + firebase')
     await executeCommand(`cd ${rootFolder}/android && fastlane default_setapp theme_folder:${themeName} && echo "Hora de criar o commit..." && cd ${themeFolder} && git add --all && git commit -m "launch app" && git push origin ${account} `);
+    console.log('Ligando o react Native');
+    await executeCommand(`ttab "cd ${rootFolder}/mbContent/themes/${themeName}; millbody start"`);
+    console.log('Reabrindo xcode...');
+    await executeCommand(`cd ${rootFolder}/ios && xed .`)
     console.log('Criando os apps nas lojas...')
-    console.log('Building iOS...')
-    await executeCommand(`cd ${rootFolder}/ios && fastlane ${options.track === 'prod' ? 'deploy' : 'beta'} theme_folder:${themeName} && echo "Building Andoid..." && cd ${rootFolder}/android && fastlane ${options.track === 'prod' ? 'deploy' : 'beta'} theme_folder:${themeName}`);
+    console.log('Building Android...')
+    await executeCommand(`cd ${rootFolder}/android && ./gradlew assembleRelease && echo "Building Andoid..." && cd ${rootFolder}/ios && fastlane ${options.track === 'prod' ? 'deploy' : 'create_app'} theme_folder:${themeName}`);
 }
